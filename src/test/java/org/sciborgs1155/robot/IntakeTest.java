5c07e43da0c5756862029b7dc0b93f68cea3ffd2
dannynotsmart <75951607+dannynotsmart@users.noreply.github.com>
2024-01-23 16:35:26 -0500

    test

--- src/test/java/org/sciborgs1155/robot/IntakeTest.java
+++ src/test/java/org/sciborgs1155/robot/IntakeTest.java
@@ -1,29 +1,11 @@
 package org.sciborgs1155.robot;
 
-import static org.sciborgs1155.lib.TestingUtil.*;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.sciborgs1155.robot.intake.Intake;
 
 public class IntakeTest {
-  Intake intake;
-
-  @BeforeEach
-  public void setup() {
-    setupHAL();
-    intake = new Intake();
-  }
-
   @Test
-  public void spinMotor() {
-    run(intake.spin(true));
-    fastForward();
-  }
-
-  @AfterEach
-  public void close() throws Exception {
-    intake.close();
+  public void init() throws Exception {
+    new Intake().close();
   }
 }
