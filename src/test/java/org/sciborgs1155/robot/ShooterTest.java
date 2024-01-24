3c73f8c6d59c49caa827b751324261b44f7fe942
yxhej <wui@bxscience.edu>
2024-01-24 15:55:04 -0500

    shooter changes

--- src/test/java/org/sciborgs1155/robot/ShooterTest.java
+++ src/test/java/org/sciborgs1155/robot/ShooterTest.java
@@ -2,37 +2,42 @@ package org.sciborgs1155.robot;
 
 import static edu.wpi.first.units.Units.Radians;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.sciborgs1155.lib.TestingUtil.*;
-
-import java.util.function.Supplier;
+import static org.sciborgs1155.lib.TestingUtil.fastForward;
+import static org.sciborgs1155.lib.TestingUtil.run;
+import static org.sciborgs1155.lib.TestingUtil.setupHAL;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.sciborgs1155.robot.shooter.Shooter;
-
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.units.Angle;
-import edu.wpi.first.units.Measure;
+import org.sciborgs1155.robot.shooter.feeder.SimFeeder;
+import org.sciborgs1155.robot.shooter.flywheel.SimFlywheel;
+import org.sciborgs1155.robot.shooter.pivot.SimPivot;
 
 public class ShooterTest {
   Shooter shooter;
+  SimPivot pivot;
+  SimFlywheel flywheel;
+  SimFeeder feeder;
+  final double DELTA = 1e-2;
 
   @BeforeEach
   public void setup() {
     setupHAL();
-    shooter = Shooter.create();
+    pivot = new SimPivot();
+    flywheel = new SimFlywheel();
+    feeder = new SimFeeder();
+    shooter = new Shooter(flywheel, pivot, feeder);
   }
 
   @Test
   public void testFlywheel() {
     run(shooter.runFlywheel(() -> 3));
-    run(shooter.runPivot(() -> Radians.of(Math.PI/4)));
+    run(shooter.runPivot(() -> Radians.of(Math.PI / 4)));
     run(shooter.runFeeder(-0.4));
     fastForward(1000);
 
-    System.out.println(shooter.getFlywheelVelocity());
-    assertEquals(3, shooter.getFlywheelVelocity());
-    assertEquals(300, shooter.getPivotPosition());
-    assertEquals(-0.4, shooter.getFeederVelocity());
+    assertEquals(3, flywheel.getVelocity(), DELTA);
+    assertEquals(300, pivot.getPosition(), DELTA);
+    assertEquals(-0.4, feeder.getVelocity(), DELTA);
   }
 }
