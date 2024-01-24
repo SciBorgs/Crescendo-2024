8512d4cafb3674839db33b319acaf9a987baaef4
Andrew Herrera <andrewhts123x@gmail.com>
2024-01-24 16:22:00 -0500

    Add rear and front cameras along with respective pose estimators

---
+++ src/main/java/org/sciborgs1155/robot/vision/Vision.java
@@ -0,0 +1,162 @@
+package org.sciborgs1155.robot.vision;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.simulation.PhotonCameraSim;
+import org.photonvision.simulation.SimCameraProperties;
+import org.photonvision.simulation.VisionSystemSim;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.sciborgs1155.robot.Robot;
+
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import org.sciborgs1155.robot.vision.VisionConstants;
+public class Vision {
+    private final PhotonCamera frontCamera;
+    private final PhotonCamera rearCamera;
+    private final PhotonPoseEstimator frontPhotonEstimator;
+    private final PhotonPoseEstimator rearPhotonEstimator;
+    private double lastEstTimestamp = 0;
+
+    // Simulation
+    private PhotonCameraSim cameraSim;
+    private VisionSystemSim visionSim;
+
+    public Vision() {
+        this.frontCamera = new PhotonCamera(VisionConstants.FRONT_CAMERA_NAME);
+        this.rearCamera = new PhotonCamera(VisionConstants.REAR_CAMERA_NAME);
+
+        
+        frontPhotonEstimator = new PhotonPoseEstimator(
+            VisionConstants.TAG_LAYOUT, 
+            PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, 
+            frontCamera, 
+            VisionConstants.ROBOT_TO_CAM);
+
+        frontPhotonEstimator.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
+        rearPhotonEstimator =
+                new PhotonPoseEstimator(
+                        VisionConstants.TAG_LAYOUT, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, rearCamera, VisionConstants.ROBOT_TO_CAM);
+        rearPhotonEstimator.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
+
+
+        // ----- Simulation
+        if (Robot.isSimulation()) {
+            // Create the vision system simulation which handles cameras and targets on the field.
+            visionSim = new VisionSystemSim("main");
+            // Add all the AprilTags inside the tag layout as visible targets to this simulated field.
+            visionSim.addAprilTags(VisionConstants.TAG_LAYOUT);
+            // Create simulated camera properties. These can be set to mimic your actual camera.
+            var cameraProp = new SimCameraProperties();
+            cameraProp.setCalibration(960, 720, Rotation2d.fromDegrees(90));
+            cameraProp.setCalibError(0.35, 0.10);
+            cameraProp.setFPS(15);
+            cameraProp.setAvgLatencyMs(50);
+            cameraProp.setLatencyStdDevMs(15);
+            // Create a PhotonCameraSim which will update the linked PhotonCamera's values with visible
+            // targets.
+            cameraSim = new PhotonCameraSim(frontCamera, cameraProp);
+            // Add the simulated camera to view the targets on this simulated field.
+            visionSim.addCamera(cameraSim,VisionConstants.ROBOT_TO_CAM);
+
+            cameraSim.enableDrawWireframe(true);
+        }
+    }
+
+
+
+    /**
+     * The latest estimated robot pose on the field from vision data. This may be empty. This should
+     * only be called once per loop.
+     *
+     * @return An {@link EstimatedRobotPose} with an estimated pose, estimate timestamp, and targets
+     *     used for estimation.
+     */
+    public Optional<EstimatedRobotPose> getEstimatedGlobalPose(PhotonPoseEstimator poseEstimator, PhotonCamera camera) {
+        var visionEst = poseEstimator.update();
+
+        double latestTimestamp = camera.getLatestResult().getTimestampSeconds();
+        boolean newResult = Math.abs(latestTimestamp - lastEstTimestamp) > 1e-5;
+        if (Robot.isSimulation()) {
+            visionEst.ifPresentOrElse(
+                    est ->
+                            getSimDebugField()
+                                    .getObject("VisionEstimation")
+                                    .setPose(est.estimatedPose.toPose2d()),
+                    () -> {
+                        if (newResult) getSimDebugField().getObject("VisionEstimation").setPoses();
+                    });
+        }
+        if (newResult) lastEstTimestamp = latestTimestamp;
+        return visionEst;
+    }
+    public EstimatedRobotPose[] getEstimatedGlobalPoses() {
+        
+    
+        return Stream.of(frontPhotonEstimator, rearPhotonEstimator)
+            .map(PhotonPoseEstimator::update)
+            .flatMap(Optional::stream)
+            .toArray(EstimatedRobotPose[]::new);
+    }
+
+    
+
+
+    /**
+     * The standard deviations of the estimated pose from {@link #getEstimatedGlobalPose()}, for use
+     * with {@link edu.wpi.first.math.estimator.SwerveDrivePoseEstimator SwerveDrivePoseEstimator}.
+     * This should only be used when there are targets visible.
+     *
+     * @param estimatedPose The estimated pose to guess standard deviations for.
+     */
+    public Matrix<N3, N1> getEstimationStdDevs(Pose2d estimatedPose, PhotonCamera camera, PhotonPoseEstimator estimator) {
+        var estStdDevs = VisionConstants.SINGLE_TAG_STD_DEVS;
+        var targets = camera.getLatestResult().getTargets();
+        int numTags = 0;
+        double avgDist = 0;
+        for (var tgt : targets) {
+            var tagPose = estimator.getFieldTags().getTagPose(tgt.getFiducialId());
+            if (tagPose.isEmpty()) continue;
+            numTags++;
+            avgDist +=
+                    tagPose.get().toPose2d().getTranslation().getDistance(estimatedPose.getTranslation());
+        }
+        if (numTags == 0) return estStdDevs;
+        avgDist /= numTags;
+        // Decrease std devs if multiple targets are visible
+        if (numTags > 1) estStdDevs = VisionConstants.MULTIPLE_TAG_STD_DEVS;
+        // Increase std devs based on (average) distance
+        if (numTags == 1 && avgDist > 4)
+            estStdDevs = VecBuilder.fill(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
+        else estStdDevs = estStdDevs.times(1 + (avgDist * avgDist / 30));
+
+        return estStdDevs;
+    }
+
+    // ----- Simulation
+
+    public void simulationPeriodic(Pose2d robotSimPose) {
+        visionSim.update(robotSimPose);
+    }
+
+    /** Reset pose history of the robot in the vision system simulation. */
+    public void resetSimPose(Pose2d pose) {
+        if (Robot.isSimulation()) visionSim.resetRobotPose(pose);
+    }
+
+    /** A Field2d for visualizing our robot and objects on the field. */
+    public Field2d getSimDebugField() {
+        if (!Robot.isSimulation()) return null;
+        return visionSim.getDebugField();
+    }
+}
