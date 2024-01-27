8d9a59230cdbecba2be3957e2894a96a63a13ba7
Asa Paparo <asapaparo@gmail.com>
2024-01-27 00:38:30 -0500

    spotless

--- src/main/java/org/sciborgs1155/robot/vision/VisionConstants.java
+++ src/main/java/org/sciborgs1155/robot/vision/VisionConstants.java
@@ -9,23 +9,20 @@ import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
-import edu.wpi.first.math.util.Units;
-import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 
 public class VisionConstants {
-    public static final AprilTagFieldLayout TAG_LAYOUT = null;
+  public static final AprilTagFieldLayout TAG_LAYOUT = null;
 
-    public static final String FRONT_CAMERA_NAME= "placeholder";
-    public static final String REAR_CAMERA_NAME = "placeholder";
-    public static final Transform3d ROBOT_TO_CAM =
-                new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0, 0, 0));
+  public static final String FRONT_CAMERA_NAME = "placeholder";
+  public static final String REAR_CAMERA_NAME = "placeholder";
+  public static final Transform3d ROBOT_TO_CAM =
+      new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0, 0, 0));
 
-        // The layout of the AprilTags on the field
-    public static final AprilTagFieldLayout kTagLayout =
-                AprilTagFields.kDefaultField.loadAprilTagLayoutField();
+  // The layout of the AprilTags on the field
+  public static final AprilTagFieldLayout kTagLayout =
+      AprilTagFields.kDefaultField.loadAprilTagLayoutField();
 
-        // The standard deviations of
-    public static final Matrix<N3, N1> SINGLE_TAG_STD_DEVS = VecBuilder.fill(4, 4, 8);
-    public static final Matrix<N3, N1> MULTIPLE_TAG_STD_DEVS = VecBuilder.fill(4, 4, 8);
-
+  // The standard deviations of
+  public static final Matrix<N3, N1> SINGLE_TAG_STD_DEVS = VecBuilder.fill(4, 4, 8);
+  public static final Matrix<N3, N1> MULTIPLE_TAG_STD_DEVS = VecBuilder.fill(4, 4, 8);
 }
