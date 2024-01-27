5df6a9b1af6bb5ddd4415c19d4ac841dc226adec
HenryLi-0 <bxsci8266@gmail.com>
2024-01-27 11:47:20 -0500

    removed unused LedIO.java

--- src/main/java/org/sciborgs1155/robot/led/LedIO.java
+++
@@ -1,8 +0,0 @@
-package org.sciborgs1155.robot.led;
-
-import edu.wpi.first.wpilibj.AddressableLEDBuffer;
-import monologue.Logged;
-
-public interface LedIO extends Logged, AutoCloseable {
-  void setData(AddressableLEDBuffer ledBuffer);
-}
