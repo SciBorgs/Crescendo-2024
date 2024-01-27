d18872bcef6fc192fc427c2921ddf5d20a4b3ad4
jonahk17 <jonahkwon@jonahs-air-2.78x445.nycboe.org>
2024-01-27 15:57:18 -0500

    math for while moving (doesn't currently work)

--- aios/trajectory.py
+++ aios/trajectory.py
@@ -1,24 +1,30 @@
 import casadi
 import math
 
-class trajectory:
+class Trajectory:
     """
     Represents a trajectory object. 
     Returns an optimal setting of angle relative to horizontal
     and shooter launch speed to send the note into the speaker.
 
+    The origin has been chosen as the center of the speaker-back that is being scored into. "x-axis" is parallel to side that speaker is attached to.
+
     ...
 
     Attributes
     ----------
-    distanceToTarget : str
-        The distance from the robot to the center of the start of the speaker
-    currentSpeedState : float
-        The current speed at which the shooter will shoot the note (in m/s)
+    x : float
+        The x-coordinate of the robot at the time of launch
+    y: float
+        The y-coordinate of the robot at the time of launch.
+    vx: float
+        The change in the x-coordinate with respect to time at launch time.
+    vy : float
+        The change in the y-coordinate with respect to time at launch time.
+    launchSpeed : float
+        The current launch speed of the shooter.
     currentAngleState : float
         The angle relative to the horizontal that the shooter is inclined at. 
-    currentHeadingState : float
-        The angle formed by the edge of the field containing the target speaker and the center of the edge of the speaker to the robot. 
     angleWeight : float
         The weight in the loss function of a change in angle
     speedWeight : float
@@ -27,91 +33,90 @@ class trajectory:
     Methods 
     -------
     getNewShooterState()
-        Returns a dict of a new rotational speed and angle for the motor to be.
+        Returns a dict of a new state the motor should be in. (Launch speed and angle of inclination relative to floor).
     """
-    def __init__(self, distanceToTarget:float, currentSpeedState:float, currentAngleState:float, currentHeadingState:float, angleWeight:float, speedWeight:float):
+    def __init__(self, x:float, y:float, vx:float, vy:float, launchSpeed : float, currentAngleState:float, angleWeight:float, speedWeight:float):
         """
         Parameters
         ----------
-        distanceToTarget : str
-            The distance from the robot to the center of the start of the speaker
-        currentSpeedState : float
-            The current launch speed of the shooter
+        x : float
+            The x-coordinate of the robot at the time of launch
+        y: float
+            The y-coordinate of the robot at the time of launch.
+        vx: float
+            The change in the x-coordinate with respect to time at launch time.
+        vy : float
+            The change in the y-coordinate with respect to time at launch time.
+        launchSpeed : float
+            The current launch speed of the shooter.
         currentAngleState : float
             The angle relative to the horizontal that the shooter is inclined at. 
-        currentHeadingState : float
-            The angle formed by the edge of the field containing the target speaker and the center of the edge of the speaker to the robot. 
         angleWeight : float
             The weight in the loss function of a change in angle
         speedWeight : float
             The weight in the loss function of a change in shooter launching speed.
-        """
-    
 
-        self.distanceToTarget = distanceToTarget
-        self.currentSpeedState = currentSpeedState
+        """
+
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+        self.launchSpeed = launchSpeed
         self.currentAngleState = currentAngleState
-        self.currentHeadingState = currentHeadingState
         self.angleWeight = angleWeight
         self.speedWeight = speedWeight
         self.MIN_HEIGHT = 1.98 #m
         self.MAX_HEIGHT = 2.11 #m
         self.GRAVITATIONAL_ACCELERATION = 9.805 #m/s/s
         self.OPENING_GAP = 0.46 #m
+        self.SPEAKER_WIDTH = 1.05 #m
+        self.NOTE_OUTER_RADIUS = 0.36 #m
+        self.NOTE_THICKNESS = 0.06 #m
+        self.MIN_HEADING = math.atan(self.OPENING_GAP/ ( (self.SPEAKER_WIDTH / 2) - self.NOTE_OUTER_RADIUS ) )
 
-
-
-    
     def getNewShooterState(self):
         """
         Returns a dict of a new rotational speed and angle for the motor to be.
         """
         
+        #Get heading relative to center of speaker back
+        try: 
+            heading = math.atan(self.y / self.x )
+        
+        except ZeroDivisionError:
+            heading = math.pi / 2
+            print(heading)
+    
+        if heading < self.MIN_HEADING:
+            return ValueError("No launch properties at this heading available.")
+
         #Variable initialization
         opti = casadi.Opti()
         deltaV = opti.variable()
         deltaTheta = opti.variable()
 
+        loss = ( self.angleWeight * deltaTheta ** 2) + (self.speedWeight * deltaV ** 2)
         opti.minimize( 
-           ( self.angleWeight * deltaTheta ** 2) + (self.speedWeight * deltaV ** 2)
+           loss
         )
 
         opti.subject_to(
-            self.MIN_HEIGHT < ((self.distanceToTarget + self.OPENING_GAP) * casadi.tan(self.currentAngleState + deltaTheta))
-            -  ( (self.GRAVITATIONAL_ACCELERATION * (self.distanceToTarget + self.OPENING_GAP) ** 2) / 
-              (2 * ( (self.currentSpeedState + deltaV) ** 2) * (casadi.cos(self.currentAngleState + deltaTheta) ** 2) )
-            )
-        )
-        opti.subject_to(
-            self.MAX_HEIGHT > (self.distanceToTarget * casadi.tan(self.currentAngleState + deltaTheta))
-            -  ( (self.GRAVITATIONAL_ACCELERATION * self.distanceToTarget ** 2) / 
-              (2 * ( (self.currentSpeedState + deltaV) ** 2) * (casadi.cos(self.currentAngleState + deltaTheta) ** 2) )
-            )
+            self.MAX_HEIGHT - self.NOTE_THICKNESS < (self.launchSpeed + deltaV) * casadi.sin(deltaTheta + self.currentAngleState) * ((self.y - self.OPENING_GAP) / ( (self.launchSpeed + deltaV) * casadi.cos(deltaTheta + self.currentAngleState) * math.sin(heading) + self.vy)) - 
+            0.5 * self.GRAVITATIONAL_ACCELERATION * ((self.y - self.OPENING_GAP)/ ( (self.launchSpeed + deltaV) * casadi.cos(deltaTheta + self.currentAngleState) * math.sin(heading) + self.vy))
         )
 
         opti.subject_to(
-            0 < (casadi.tan(self.currentAngleState + deltaTheta))
-            -  ( (self.GRAVITATIONAL_ACCELERATION * self.distanceToTarget) / 
-              (( (self.currentSpeedState + deltaV) ** 2) * (casadi.cos(self.currentAngleState + deltaTheta) ** 2) )
-            )
+            self.MIN_HEIGHT + self.NOTE_THICKNESS > (self.launchSpeed + deltaV) * casadi.sin(deltaTheta + self.currentAngleState) * ((self.y) / ( (self.launchSpeed + deltaV) * casadi.cos(deltaTheta + self.currentAngleState) * math.sin(heading) + self.vy)) - 
+            0.5 * self.GRAVITATIONAL_ACCELERATION * ((self.y)/ ( (self.launchSpeed + deltaV) * casadi.cos(deltaTheta + self.currentAngleState) * math.sin(heading) + self.vy))
         )
-
-        opti.subject_to(self.currentSpeedState + deltaV < 30)
-        
 
         opti.solver('ipopt')
+
         solutions = opti.solve()
+    
 
         return {
             'speed' : round(solutions.value(deltaV) + self.currentSpeedState, 3), 
             'angle' : round(solutions.value(deltaTheta) + self.currentAngleState, 3),
         }
-
-
-
-    
-
-
-
-
-
