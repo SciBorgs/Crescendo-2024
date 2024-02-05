bd9c0ebd5d0b084dd2c1e7c938d5377f4c8c26a8
michaelx0281 <69123340+michaelx0281@users.noreply.github.com>
2024-02-05 16:57:07 -0500

    added sysid routines to feeder

--- src/main/java/org/sciborgs1155/robot/feeder/Feeder.java
+++ src/main/java/org/sciborgs1155/robot/feeder/Feeder.java
@@ -1,22 +1,46 @@
 package org.sciborgs1155.robot.feeder;
 
+import static edu.wpi.first.units.Units.Volts;
 import static org.sciborgs1155.robot.feeder.FeederConstants.*;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import monologue.Logged;
+import monologue.Annotations.Log;
+
 import org.sciborgs1155.robot.Robot;
 
 public class Feeder extends SubsystemBase implements AutoCloseable, Logged {
   private final FeederIO feeder;
 
+  @Log.NT
   private final PIDController pid = new PIDController(kP, kI, kD);
   private final SimpleMotorFeedforward ff = new SimpleMotorFeedforward(kS, kV, kA);
 
+  private final SysIdRoutine sysId;
+
   public Feeder(FeederIO feeder) {
     this.feeder = feeder;
+    sysId = 
+      new SysIdRoutine(
+        new SysIdRoutine.Config(),
+        new SysIdRoutine.Mechanism(
+          v -> feeder.set(v.in(Volts)),
+          null, 
+          this,
+          "Feeder")
+      );
+
+    SmartDashboard.putData("feeder quasistatic backward", quasistaticBack());
+    SmartDashboard.putData("feeder quasistatic forward", quasistaticForward());
+    SmartDashboard.putData("feeder dynamic backward", dynamicBack());
+    SmartDashboard.putData("feeder dynamic forward", dynamicForward());
+
   }
 
   public static Feeder create() {
@@ -27,6 +51,22 @@ public class Feeder extends SubsystemBase implements AutoCloseable, Logged {
     return run(() ->
             feeder.set(pid.calculate(feeder.getVelocity(), voltage) + ff.calculate(voltage)))
         .withName("running feeder, " + voltage + " volts");
+  }
+
+  public Command quasistaticBack() {
+    return sysId.quasistatic(Direction.kReverse);
+  }
+
+  public Command quasistaticForward() {
+    return sysId.quasistatic(Direction.kForward);
+  }
+
+  public Command dynamicForward() {
+    return sysId.dynamic(Direction.kForward);
+  }
+
+  public Command dynamicBack() {
+    return sysId.dynamic(Direction.kReverse);
   }
 
   @Override
