e5f7971655871f96893d3dd211f6756a05397675
SciBorgs User <roboticsbxsci@gmail.com>
2024-02-06 16:31:09 -0500

    kishan

--- src/main/java/org/sciborgs1155/robot/Robot.java
+++ src/main/java/org/sciborgs1155/robot/Robot.java
@@ -1,36 +1,11 @@
 package org.sciborgs1155.robot;
 
-import static edu.wpi.first.units.Units.*;
-import static edu.wpi.first.wpilibj2.command.button.RobotModeTriggers.*;
-import static org.sciborgs1155.robot.shooter.ShooterConstants.FlywheelConstants.PRESET_SUBWOOFER_ANGLE;
-import static org.sciborgs1155.robot.shooter.ShooterConstants.FlywheelConstants.PRESET_SUBWOOFER_VELOCITY;
-
-import com.pathplanner.lib.auto.AutoBuilder;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.DataLogManager;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.GenericHID.RumbleType;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.ProxyCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import monologue.Annotations.Log;
 import monologue.Logged;
-import monologue.Monologue;
-import org.littletonrobotics.urcl.URCL;
 import org.sciborgs1155.lib.CommandRobot;
-import org.sciborgs1155.lib.FaultLogger;
-import org.sciborgs1155.lib.InputStream;
 import org.sciborgs1155.robot.Ports.OI;
-import org.sciborgs1155.robot.drive.Drive;
-import org.sciborgs1155.robot.drive.DriveConstants;
 import org.sciborgs1155.robot.led.LedStrip;
 import org.sciborgs1155.robot.led.LedStrip.LEDTheme;
-import org.sciborgs1155.robot.shooter.Shooting;
-import org.sciborgs1155.robot.shooter.feeder.Feeder;
-import org.sciborgs1155.robot.shooter.flywheel.Flywheel;
-import org.sciborgs1155.robot.shooter.pivot.Pivot;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -42,22 +17,22 @@ public class Robot extends CommandRobot implements Logged {
 
   // INPUT DEVICES
   private final CommandXboxController operator = new CommandXboxController(OI.OPERATOR);
-  private final CommandXboxController driver = new CommandXboxController(OI.DRIVER);
+  // private final CommandXboxController driver = new CommandXboxController(OI.DRIVER);
 
   // SUBSYSTEMS
-  @Log.NT private final Drive drive = Drive.create();
+  // @Log.NT private final Drive drive = Drive.create();
 
-  private final Flywheel flywheel = Flywheel.create();
-  private final Feeder feeder = Feeder.create();
-  private final Pivot pivot = Pivot.create();
+  // private final Flywheel flywheel = Flywheel.create();
+  // private final Feeder feeder = Feeder.create();
+  // private final Pivot pivot = Pivot.create();
   private final LedStrip led = new LedStrip();
 
   // COMMANDS
-  @Log.NT private final SendableChooser<Command> autos = AutoBuilder.buildAutoChooser();
+  // @Log.NT private final SendableChooser<Command> autos = AutoBuilder.buildAutoChooser();
 
-  @Log.NT private final Shooting shooting = new Shooting(flywheel, pivot, feeder);
+  // @Log.NT private final Shooting shooting = new Shooting(flywheel, pivot, feeder);
 
-  @Log.NT private double speedMultiplier = Constants.FULL_SPEED_MULTIPLIER;
+  // @Log.NT private double speedMultiplier = Constants.FULL_SPEED_MULTIPLIER;
 
   /** The robot contains subsystems, OI devices, and commands. */
   public Robot() {
@@ -69,50 +44,50 @@ public class Robot extends CommandRobot implements Logged {
 
   /** Configures basic behavior during different parts of the game. */
   private void configureGameBehavior() {
-    // Configure logging with DataLogManager, Monologue, FailureManagement, and URCL
-    DataLogManager.start();
-    Monologue.setupMonologue(this, "/Robot", false, true);
-    addPeriodic(Monologue::updateAll, kDefaultPeriod);
-    FaultLogger.setupLogging();
-    addPeriodic(FaultLogger::update, 1);
+    // // Configure logging with DataLogManager, Monologue, FailureManagement, and URCL
+    // DataLogManager.start();
+    // Monologue.setupMonologue(this, "/Robot", false, true);
+    // addPeriodic(Monologue::updateAll, kDefaultPeriod);
+    // FaultLogger.setupLogging();
+    // addPeriodic(FaultLogger::update, 1);
 
-    if (isReal()) {
-      URCL.start();
-    } else {
-      DriverStation.silenceJoystickConnectionWarning(true);
-    }
+    // if (isReal()) {
+    //   URCL.start();
+    // } else {
+    //   DriverStation.silenceJoystickConnectionWarning(true);
+    // }
   }
 
   /** Creates an input stream for a joystick. */
-  private InputStream createJoystickStream(InputStream input, double maxSpeed, double maxRate) {
-    return input
-        .deadband(Constants.DEADBAND, 1)
-        .negate()
-        .scale(maxSpeed)
-        .scale(() -> speedMultiplier)
-        .signedPow(2)
-        .rateLimit(maxRate);
-  }
+  // private InputStream createJoystickStream(InputStream input, double maxSpeed, double maxRate) {
+  //   return input
+  //       .deadband(Constants.DEADBAND, 1)
+  //       .negate()
+  //       .scale(maxSpeed)
+  //       .scale(() -> speedMultiplier)
+  //       .signedPow(2)
+  //       .rateLimit(maxRate);
+  // }
 
   /**
    * Configures subsystem default commands. Default commands are scheduled when no other command is
    * running on a subsystem.
    */
   private void configureSubsystemDefaults() {
-    drive.setDefaultCommand(
-        drive.drive(
-            createJoystickStream(
-                driver::getLeftX,
-                DriveConstants.MAX_SPEED.in(MetersPerSecond),
-                DriveConstants.MAX_ACCEL.in(MetersPerSecondPerSecond)),
-            createJoystickStream(
-                driver::getLeftY,
-                DriveConstants.MAX_SPEED.in(MetersPerSecond),
-                DriveConstants.MAX_ACCEL.in(MetersPerSecondPerSecond)),
-            createJoystickStream(
-                driver::getRightX,
-                DriveConstants.MAX_ANGULAR_SPEED.in(RadiansPerSecond),
-                DriveConstants.MAX_ANGULAR_ACCEL.in(RadiansPerSecond.per(Second)))));
+    // drive.setDefaultCommand(
+    //     drive.drive(
+    //         createJoystickStream(
+    //             driver::getLeftX,
+    //             DriveConstants.MAX_SPEED.in(MetersPerSecond),
+    //             DriveConstants.MAX_ACCEL.in(MetersPerSecondPerSecond)),
+    //         createJoystickStream(
+    //             driver::getLeftY,
+    //             DriveConstants.MAX_SPEED.in(MetersPerSecond),
+    //             DriveConstants.MAX_ACCEL.in(MetersPerSecondPerSecond)),
+    //         createJoystickStream(
+    //             driver::getRightX,
+    //             DriveConstants.MAX_ANGULAR_SPEED.in(RadiansPerSecond),
+    //             DriveConstants.MAX_ANGULAR_ACCEL.in(RadiansPerSecond.per(Second)))));
   }
 
   /** Registers all named commands, which will be used by pathplanner */
@@ -122,36 +97,38 @@ public class Robot extends CommandRobot implements Logged {
 
   /** Configures trigger -> command bindings */
   private void configureBindings() {
-    autonomous().whileTrue(new ProxyCommand(autos::getSelected));
-    FaultLogger.onFailing(f -> Commands.print(f.toString()));
+    // autonomous().whileTrue(new ProxyCommand(autos::getSelected));
+    // FaultLogger.onFailing(f -> Commands.print(f.toString()));
 
-    driver
-        .leftBumper()
-        .or(driver.rightBumper())
-        .onTrue(Commands.runOnce(() -> speedMultiplier = Constants.FULL_SPEED_MULTIPLIER))
-        .onFalse(Commands.run(() -> speedMultiplier = Constants.SLOW_SPEED_MULTIPLIER));
+    // driver
+    //     .leftBumper()
+    //     .or(driver.rightBumper())
+    //     .onTrue(Commands.runOnce(() -> speedMultiplier = Constants.FULL_SPEED_MULTIPLIER))
+    //     .onFalse(Commands.run(() -> speedMultiplier = Constants.SLOW_SPEED_MULTIPLIER));
 
-    operator.a().toggleOnTrue(pivot.manualPivot(operator::getLeftY));
+    // operator.a().toggleOnTrue(pivot.manualPivot(operator::getLeftY));
 
-    shooting
-        .canShoot()
-        .onTrue(led.setLEDTheme(LEDTheme.CANSHOOT))
-        .onFalse(led.setLEDTheme(LEDTheme.FIRE));
+    // shooting
+    //     .canShoot()
+    //     .onTrue(led.setLEDTheme(LEDTheme.CANSHOOT))
+    //     .onFalse(led.setLEDTheme(LEDTheme.FIRE));
 
-    // shooting into speaker when up to subwoofer
-    operator
-        .x()
-        .and(shooting.canShoot())
-        .toggleOnTrue(
-            shooting.pivotThenShoot(
-                () -> new Rotation2d(PRESET_SUBWOOFER_ANGLE),
-                () -> PRESET_SUBWOOFER_VELOCITY.in(RadiansPerSecond)));
+    // // shooting into speaker when up to subwoofer
+    // operator
+    //     .x()
+    //     .and(shooting.canShoot())
+    //     .toggleOnTrue(
+    //         shooting.pivotThenShoot(
+    //             () -> new Rotation2d(PRESET_SUBWOOFER_ANGLE),
+    //             () -> PRESET_SUBWOOFER_VELOCITY.in(RadiansPerSecond)));
 
-    // assuming x is shoot button, will rumble if cant shoot
-    operator
-        .x()
-        .and(() -> shooting.canShoot().getAsBoolean() == false)
-        .toggleOnTrue(rumble(RumbleType.kBothRumble, 0.5));
+    // // assuming x is shoot button, will rumble if cant shoot
+    // operator
+    //     .x()
+    //     .and(() -> shooting.canShoot().getAsBoolean() == false)
+    //     .toggleOnTrue(rumble(RumbleType.kBothRumble, 0.5));
+
+    operator.a().toggleOnTrue(led.setLEDTheme(LEDTheme.RAINBOW));
   }
 
   @Override
@@ -160,7 +137,7 @@ public class Robot extends CommandRobot implements Logged {
     super.close();
   }
 
-  public Command rumble(RumbleType rumbleType, double strength) {
-    return Commands.run(() -> operator.getHID().setRumble(rumbleType, strength));
-  }
+  // public Command rumble(RumbleType rumbleType, double strength) {
+  //   return Commands.run(() -> operator.getHID().setRumble(rumbleType, strength));
+  // }
 }
