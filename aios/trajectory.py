8c24d41ee556f6379a09bb27edc5e9927e938f44
jonahk17 <jonahkwon@Jonahs-MacBook-Air-2.local>
2024-02-09 23:19:45 -0500

    Remove casadi

--- aios/trajectory.py
+++ aios/trajectory.py
@@ -1,186 +1,75 @@
-import casadi
-import math
+import numpy as np
+from scipy.optimize import fsolve
 
 class Trajectory:
     """
-    Represents a trajectory object. 
-    Returns an optimal setting of angle relative to horizontal
-    and shooter launch speed to send the note into the speaker.
-
-    The origin has been chosen as the center of the speaker-back that is being scored into. "x-axis" is parallel to side that speaker is attached to.
+    Represents a trajectory object
 
     ...
 
     Attributes
     ----------
-    x : float
-        The x-coordinate of the robot at the time of launch
-    y: float
+    posX : float
+        The x-coordinate of the robot at the time of launch.
+    posY : float
         The y-coordinate of the robot at the time of launch.
-    vx: float
-        The change in the x-coordinate with respect to time at launch time.
+    vx : float
+        The velocity in the x-direction of the robot at the time of launch.
     vy : float
-        The change in the y-coordinate with respect to time at launch time.
-    phi: float
-        The current angle that the shooter is at with respect to the y-axis
-    v : float
-        The current launch speed of the shooter.
-    theta : float
-        The angle relative to the horizontal that the shooter is inclined at. 
-    angleWeight : float
-        The weight in the loss function of a change in angle
-    speedWeight : float
-        The weight in the loss function of a change in shooter launching speed.
-    headingWeight : float
-        The weight in the loss function of a change in shooter heading.
+        The velocity in the y-direction of the robot at the time of launch.
 
-
-    Methods 
+    Methods
     -------
-    getNewShooterState()
-        Returns a dict of a new state the motor should be in. (Launch speed and angle of inclination relative to floor).
+    returnTrajectorySettingsGivenState()
+        Returns the settings for the launcher to launch the note.
+    results()
+        Returns the settings if they are valid.
     """
-    def __init__(self, x:float, y:float, vx:float, vy:float, phi:float, v : float, theta:float, angleWeight:float, speedWeight:float, headingWeight:float):
-        """
-        Parameters
-        ----------
-        x : float
-            The x-coordinate of the robot at the time of launch.
-        y: float
-            The y-coordinate of the robot at the time of launch.
-        vx: float
-            The change in the x-coordinate with respect to time at launch time.
-        vy : float
-            The change in the y-coordinate with respect to time at launch time.
-        phi: float
-            The current angle that the shooter is at with respect to the y-axis. Towards the left side is negative and towards the right is positive (viewing the opening of the speaker)
-        launchSpeed : float
-            The current launch speed of the shooter.
-        currentAngleState : float
-            The angle relative to the horizontal that the shooter is inclined at. 
-        angleWeight : float
-            The weight in the loss function of a change in angle.
-        speedWeight : float
-            The weight in the loss function of a change in shooter launching speed.
-        headingWeight : float
-            The weight in the loss function of a change in shooter heading.
 
-        """
-
-        self.x = x
-        self.y = y
+    g = 9.8 #m/s/s
+    SPEAKER_LENGTH = 1.05 #m
+    SPEAKER_WIDTH = 0.46 #m
+    MAX_SPEAKER_HEIGHT = 2.11 #m
+    MIN_SPEAKER_HEIGHT = 1.98 #m
+    NOTE_THICKNESS = 0.06 #m
+    MAX_LAUNCH_SPEED = 13 #m/s
+    
+    def __init__(self, posX, posY, vx, vy):
+        self.posX = posX
+        self.posY = posY
         self.vx = vx
         self.vy = vy
-        self.phi = phi
-        self.v = v
-        self.theta = theta
-        self.angleWeight = angleWeight
-        self.speedWeight = speedWeight
-        self.headingWeight = headingWeight
 
-        self.MIN_HEIGHT = 1.98 #m
-        self.MAX_HEIGHT = 2.11 #m
-        self.GRAVITATIONAL_ACCELERATION = 9.805 #m/s/s
-        self.OPENING_GAP = 0.46 #m
-        self.SPEAKER_WIDTH = 1.05 #m
-        self.NOTE_OUTER_RADIUS = 0.36 #m
-        self.NOTE_THICKNESS = 0.06 #m
-        self.MAX_LAUNCH_SPEED = 40 #m/s
 
-    def getNewShooterState(self) -> dict:
+    #x[0]->v, x[1]->theta, x[2]->alpha
+    def _returnTrajectorySettingsGivenState(self, x):
         """
-        Returns a dict of a new rotational speed, angle, and heading for the motor to be.
+        Returns the trajectory settings for the given robot state.
+
+        Parameters
+        ----------
+        x: (numpy.ndarray)
+            An array representing the launch speed of the shooter, the angle relative to the horizontal it is inclined, and the heading relative to the front center of the speaker.
         """
-  
 
-        #Variable initialization
-        opti = casadi.Opti()
-        deltaV = opti.variable()
-        deltaTheta = opti.variable()
-        deltaPhi = opti.variable()
+        return [
+            x[0] * np.sin(x[1]) * ((self.posY - self.SPEAKER_WIDTH) / (x[0] * np.cos(x[1]) * np.sin(x[2]) + self.vy)) - (1/2) * self.g * (((self.posY - self.SPEAKER_WIDTH) / (x[0] * np.cos(x[1]) * np.sin(x[2]) + self.vy)) ** 2) - (self.MAX_SPEAKER_HEIGHT - (self.NOTE_THICKNESS / 2)),
+            x[0] * np.sin(x[1]) * ((self.posY) / (x[0] * np.cos(x[1]) * np.sin(x[2]) + self.vy)) - (1/2) * self.g * (((self.posY) / (x[0] * np.cos(x[1]) * np.sin(x[2]) + self.vy)) ** 2) - (self.MIN_SPEAKER_HEIGHT + (self.NOTE_THICKNESS / 2)),
+            (x[0] * np.cos(x[1]) * np.cos(x[2]) + self.vx) * ((self.posY - self.SPEAKER_WIDTH) / (x[0] * np.cos(x[1]) * np.sin(x[2]))),
+        ]
 
-        if self.vx == 0:
-            self.vx = 0.0001
-        if self.vy == 0:
-            self.vy = 0.0001
+    def results(self):
+        """
+        Returns the trajectory settings for the given robot state if valid.
        
-        opti.minimize( 
-           ( self.angleWeight * deltaTheta ** 2) + (self.speedWeight * deltaV ** 2) + (self.headingWeight * deltaPhi ** 2)
-        )
-
-        opti.subject_to(
-            0 < self.v + deltaV
-        )
-
-        opti.subject_to(
-            self.v + deltaV <= self.MAX_LAUNCH_SPEED
-        )
-        opti.subject_to(
-            0 < self.theta + deltaTheta 
-        )
-        
-        opti.subject_to(
-            self.theta + deltaTheta < math.pi / 2
-        )
-
-        opti.subject_to(
-            -1 * math.pi / 2 < self.phi + deltaPhi 
-        )
-
-        opti.subject_to(
-            self.phi + deltaPhi < math.pi / 2
-        )
-
-        opti.subject_to(
-            self.MAX_HEIGHT - (self.NOTE_THICKNESS / 2) >
-           ( (self.v + deltaV) * casadi.sin(deltaTheta + self.theta) * (self.y - self.OPENING_GAP)/ ( ( (self.v + deltaV) * casadi.cos(self.theta + deltaTheta) * casadi.cos(self.phi + deltaPhi) ) + self.vy ) )
-           - 0.5 * self.GRAVITATIONAL_ACCELERATION * ((self.y - self.OPENING_GAP)/ ( ( (self.v + deltaV) * casadi.cos(self.theta + deltaTheta) * casadi.cos(self.phi + deltaPhi) ) + self.vy )) ** 2
-        )
-
-        opti.subject_to(
-            self.MIN_HEIGHT + (self.NOTE_THICKNESS / 2) >
-           ( (self.v + deltaV) * casadi.sin(deltaTheta + self.theta) * (self.y)/ ( ( (self.v + deltaV) * casadi.cos(self.theta + deltaTheta) * casadi.cos(self.phi + deltaPhi) ) + self.vy ) )
-           - 0.5 * self.GRAVITATIONAL_ACCELERATION * ((self.y)/ ( ( (self.v + deltaV) * casadi.cos(self.theta + deltaTheta) * casadi.cos(self.phi + deltaPhi) ) + self.vy )) ** 2
-        )
-
-        opti.subject_to(
-            0 <=
-           ( (self.v + deltaV) * casadi.sin(deltaTheta + self.theta) * (1)/ ( ( (self.v + deltaV) * casadi.cos(self.theta + deltaTheta) * casadi.cos(self.phi + deltaPhi) ) + self.vy ) )
-           - self.GRAVITATIONAL_ACCELERATION * (self.y)/ ( ( ( (self.v + deltaV) * casadi.cos(self.theta + deltaTheta) * casadi.cos(self.phi + deltaPhi) ) + self.vy )) ** 2
-        )
-
-        opti.subject_to(
-            (-1 * self.SPEAKER_WIDTH + self.NOTE_OUTER_RADIUS) / 2 < self.x + (( (self.v + deltaV) * casadi.cos(self.theta + deltaTheta) * casadi.sin(self.phi + deltaPhi)) + self.vx) * (self.y)/ ( ( (self.v + deltaV) * casadi.cos(self.theta + deltaTheta) * casadi.cos(self.phi + deltaPhi) ) + self.vy )
-        )
-
-        opti.subject_to(
-            (self.SPEAKER_WIDTH - self.NOTE_OUTER_RADIUS) / 2 > self.x + (( (self.v + deltaV) * casadi.cos(self.theta + deltaTheta) * casadi.sin(self.phi + deltaPhi)) + self.vx) * (self.y)/ ( ( (self.v + deltaV) * casadi.cos(self.theta + deltaTheta) * casadi.cos(self.phi + deltaPhi) ) + self.vy )
-        )
-
-
-        
-        opti.solver('ipopt')
-        try:
-            solutions = opti.solve()
-        except:
-            return ValueError("No calculations for shooting available at this position.")
-
-
-        return {
-            'speed' : round(solutions.value(deltaV) + self.v, 3), 
-            'angle' : round(solutions.value(deltaTheta) + self.theta, 3),
-            'heading' : round(solutions.value(deltaPhi) + self.phi , 3)
-        }
-
-    def canShootWhileStationary(self) -> bool:
-        if (self.y < (self.OPENING_GAP / self.SPEAKER_WIDTH ) * (self.x) - (self.OPENING_GAP / 2)) and (self.y < -1 * (self.OPENING_GAP / self.SPEAKER_WIDTH ) * (self.x) - (self.OPENING_GAP / 2)):
-            return True
-        else:
-            return False
-
-    
-
-
-
+        Raises
+        ------
+        ValueError
+            If the trajectory settings are not valid.
+        """
+        solutions = fsolve(self._returnTrajectorySettingsGivenState, [3, 1, 2])
+        if solutions[0] > self.MAX_LAUNCH_SPEED:
+            raise ValueError("No calculations available for this point.")
+        return solutions
 
 
