3a593514e537d09b302177d0e60bde0001458732
HenryLi-0 <bxsci8266@gmail.com>
2024-02-10 12:06:39 -0500

    added apriltags

--- src/main/java/org/sciborgs1155/robot/vision/VisionConstants.java
+++ src/main/java/org/sciborgs1155/robot/vision/VisionConstants.java
@@ -1,9 +1,11 @@
 package org.sciborgs1155.robot.vision;
 
+import edu.wpi.first.apriltag.AprilTag;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
@@ -13,8 +15,20 @@ import edu.wpi.first.math.numbers.N3;
 import org.sciborgs1155.robot.vision.Vision.CameraConfig;
 
 public class VisionConstants {
+  // TAG POSITIONS ARE IN INCHES AHH
+  // PLEASE READ
+  // https://docs.google.com/document/d/1-q0X-9mWs7cfFUMioutAqxQHe1Zf5Aj_PK-LfFG5Yls/edit
+
+
+  private static AprilTag[] aprilTags = new AprilTag[4];
+  aprilTags[0] = new AprilTag(7, new Pose3d(28.0, 63.0, 78.0, new Rotation3d(0, 0, 0)));
+  aprilTags[1] = new AprilTag(4, new Pose3d(28.0, 100.7, 78.0, new Rotation3d(0, 0, 0)));
+  aprilTags[2] = new AprilTag(6, new Pose3d(163.0, 247.0, 48.0, new Rotation3d(0, 0, 0)));
+  aprilTags[3] = new AprilTag(7, new Pose3d(153.5, 72.0, 75.5, new Rotation3d(0, 0, 0)));
+
   public static final AprilTagFieldLayout TAG_LAYOUT =
       AprilTagFields.kDefaultField.loadAprilTagLayoutField();
+  
 
   public static final CameraConfig FRONT_CAMERA_CONFIG =
       new CameraConfig(
