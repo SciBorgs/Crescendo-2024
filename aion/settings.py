979c8262cb26c1a5b05e3c759e55d97bba8b81fc
jonahk17 <jonahkwon@Jonahs-MacBook-Air-2.local>
2024-02-15 15:12:54 -0500

    Update naming convention

--- aion/settings.py
+++ aion/settings.py
@@ -7,10 +7,16 @@ a velocity (vx, vy) with position (x, y) aiming at the center of the target spea
 """
 
 def get_shooter_state(x, y, vx, vy):
-    alpha = np.arctan(y / x) if x!=0 else np.pi / 2
+    initial_heading = np.arctan(y / x) if x!=0 else np.pi / 2
     target_v_stationary = v_approx(x, y)
     target_theta = theta_approx(x, y)
-    delta_vx = target_v_stationary * np.cos(alpha) * np.cos(target_theta) - vx
-    delta_vy = target_v_stationary * np.sin(alpha) * np.cos(target_theta) - vy
+    delta_vx = target_v_stationary * np.cos(initial_heading) * np.cos(target_theta) - vx
+    delta_vy = target_v_stationary * np.sin(initial_heading) * np.cos(target_theta) - vy
+    set_heading = initial_heading + (np.arctan(delta_vx / delta_vy) if delta_vy != 0 else 0)
     launch_speed = np.sqrt( (delta_vx ** 2 + delta_vy ** 2 + (target_v_stationary*np.sin(target_theta)) ** 2) )
-    return launch_speed, target_theta
+    return launch_speed, target_theta, set_heading
+
+
+
+
+
