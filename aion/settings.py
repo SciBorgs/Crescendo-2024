914bb32a5fd82893afe660188246d4003f2baeea
jonahk17 <jonahkwon@Jonahs-MacBook-Air-2.local>
2024-02-15 15:35:29 -0500

    Signs

--- aion/settings.py
+++ aion/settings.py
@@ -12,11 +12,11 @@ def get_shooter_state(x, y, vx, vy):
     target_theta = theta_approx(x, y)
     delta_vx = target_v_stationary * np.cos(initial_heading) * np.cos(target_theta) - vx
     delta_vy = target_v_stationary * np.sin(initial_heading) * np.cos(target_theta) - vy
-    set_heading = initial_heading + (np.arctan(delta_vx / delta_vy) if delta_vy != 0 else 0)
+    set_heading = (np.arctan(delta_vy / delta_vx) if delta_vx != 0 else 0)
     launch_speed = np.sqrt( (delta_vx ** 2 + delta_vy ** 2 + (target_v_stationary*np.sin(target_theta)) ** 2) )
     return launch_speed, target_theta, set_heading
 
-
+print(get_shooter_state(0, 4, -1, 0))
 
 
 
