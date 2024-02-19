8258dbbaa2c439e82c5a4e001713f9fde6df0a81
Asa Paparo <asapaparo@gmail.com>
2024-02-19 01:20:59 -0500

    add python formatting

--- aion/generate_coefficients.py
+++ aion/generate_coefficients.py
@@ -8,30 +8,38 @@ import multiprocessing
 Finds the coefficents for a quadratic function approximating the needed launch angle as a function of position (x,y)
 """
 
-speaker_length = 1.05 #m
-station_length = 1.75 #m
-field_length = 16.54 #m
-subwoofer_length = 0.92 #m
-min_x = round(-2 * station_length - (speaker_length / 2)) #m
-max_x = round(((speaker_length / 2) + station_length)) #m
-min_y = 0 #m
-max_y = round(field_length - subwoofer_length) #m
+speaker_length = 1.05  # m
+station_length = 1.75  # m
+field_length = 16.54  # m
+subwoofer_length = 0.92  # m
+min_x = round(-2 * station_length - (speaker_length / 2))  # m
+max_x = round(((speaker_length / 2) + station_length))  # m
+min_y = 0  # m
+max_y = round(field_length - subwoofer_length)  # m
 
-def f(variables, a, b, c, d, e, f): 
+
+def f(variables, a, b, c, d, e, f):
     x, y = variables
-    return a + b*x + c*y + d*x**2 + e*y**2 + f*x*y
+    return a + b * x + c * y + d * x**2 + e * y**2 + f * x * y
+
 
 def optimal_values(trajectory: Trajectory):
-        angle, velocity = trajectory.get_optimal_settings()
-        return (angle, velocity, trajectory.posX, trajectory.posY)
+    angle, velocity = trajectory.get_optimal_settings()
+    return (angle, velocity, trajectory.posX, trajectory.posY)
+
 
 def return_coefficients():
-    cases = [Trajectory(x, y) for y in np.arange(min_y, max_y, 0.2) for x in np.arange(min_x, max_x, 0.2)]
+    cases = [
+        Trajectory(x, y)
+        for y in np.arange(min_y, max_y, 0.2)
+        for x in np.arange(min_x, max_x, 0.2)
+    ]
     results = multiprocessing.Pool().map(optimal_values, cases)
 
     velocity_fit = curve_fit(f, (results[2], results[3]), results[0])
     pitch_fit, _ = curve_fit(f, (results[2], results[3]), results[1])
     return (velocity_fit, pitch_fit)
+
 
 # the code doesn't work without this
 if __name__ == "__main__":
--- aion/trajectory.py
+++ aion/trajectory.py
@@ -1,31 +1,29 @@
 import casadi
 import numpy as np
 
-#Define needed constants
-g = 9.8 #m/s/s
-speaker_width = 0.46 #m
-max_speaker_height = 2.11 #m
-min_speaker_height = 1.98 #m
-note_thickness = 0.06 #m
-note_outer_radius = 0.36 #m
-max_launch_speed = 13 #m/s
-speaker_length = 1.05 #m
+# Define needed constants
+g = 9.8  # m/s/s
+speaker_width = 0.46  # m
+max_speaker_height = 2.11  # m
+min_speaker_height = 1.98  # m
+note_thickness = 0.06  # m
+note_outer_radius = 0.36  # m
+max_launch_speed = 13  # m/s
+speaker_length = 1.05  # m
 
-starting_shooter_height = 0 #m
-
+starting_shooter_height = 0  # m
 
 
 class Trajectory:
     """
     Represents a trajectory to get the note into the speaker as a function of the robot position.
     It is assumed that the robot aims its shooter at the front center of the speaker (defined as the origin).
-    
-    The loss function is defined as the square of the changes in launch speed and angle, 
+
+    The loss function is defined as the square of the changes in launch speed and angle,
     from an assumed starting state of half the maximum launch speed and launch angle (45 degrees).
     Since optimization will not be performed in realtime, this provides the least amount of 'bias'
     towards any particular starting state when the robot reaches the given position (x,y).
     """
-
 
     def __init__(self, posX, posY):
         self.posX = posX
@@ -42,45 +40,50 @@ class Trajectory:
         optimizer = casadi.Opti()
         delta_theta = optimizer.variable()
         delta_v = optimizer.variable()
-        
+
         initial_v = 0.0001
         initial_theta = 0.0001
         heading = abs(np.arctan(self.posY / self.posX)) if self.posX != 0 else np.pi / 2
-        
-        t_1 = (self.posY) / ( (initial_v + delta_v) * casadi.cos(initial_theta + delta_theta) * np.sin(heading) )
-        t_2 = (self.posY + speaker_width) / ( (initial_v + delta_v) * casadi.cos(initial_theta + delta_theta) * np.sin(heading) )
+
+        t_1 = (self.posY) / (
+            (initial_v + delta_v)
+            * casadi.cos(initial_theta + delta_theta)
+            * np.sin(heading)
+        )
+        t_2 = (self.posY + speaker_width) / (
+            (initial_v + delta_v)
+            * casadi.cos(initial_theta + delta_theta)
+            * np.sin(heading)
+        )
 
         loss_function = (delta_theta) ** 2 + (delta_v) ** 2
         optimizer.minimize(loss_function)
 
         optimizer.subject_to(
-            max_speaker_height - (note_thickness / 2) > 
-            ( (initial_v + delta_v) * casadi.sin(initial_theta + delta_theta) * t_1 ) - ( (1/2) * g * t_1 ** 2)
+            max_speaker_height - (note_thickness / 2)
+            > ((initial_v + delta_v) * casadi.sin(initial_theta + delta_theta) * t_1)
+            - ((1 / 2) * g * t_1**2)
         )
 
         optimizer.subject_to(
-            min_speaker_height + (note_thickness / 2) <
-            ( (initial_v + delta_v) * casadi.sin(initial_theta + delta_theta) * t_2 ) - ( (1/2) * g * t_2 ** 2)
+            min_speaker_height + (note_thickness / 2)
+            < ((initial_v + delta_v) * casadi.sin(initial_theta + delta_theta) * t_2)
+            - ((1 / 2) * g * t_2**2)
         )
 
-        optimizer.subject_to(
-            0 < delta_v + initial_v
-        )
-        optimizer.subject_to(
-            delta_v + initial_v <= max_launch_speed
-        )
+        optimizer.subject_to(0 < delta_v + initial_v)
+        optimizer.subject_to(delta_v + initial_v <= max_launch_speed)
 
-        optimizer.subject_to(
-            0 < delta_theta + initial_theta
-        )
+        optimizer.subject_to(0 < delta_theta + initial_theta)
 
-        optimizer.subject_to(
-            delta_theta + initial_theta < np.pi / 2
-        )
+        optimizer.subject_to(delta_theta + initial_theta < np.pi / 2)
 
-        optimizer.solver('ipopt')
+        optimizer.solver("ipopt")
         try:
             solutions = optimizer.solve()
         except:
             return [0, 0]
-        return [(solutions.value(delta_v) + initial_v), (solutions.value(delta_theta) + initial_theta)]
+        return [
+            (solutions.value(delta_v) + initial_v),
+            (solutions.value(delta_theta) + initial_theta),
+        ]
