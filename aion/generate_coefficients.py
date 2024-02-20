75253677acf42222e6e0609b7add392f64aaa21b
sigalrmp <sigalrmp@gmail.com>
2024-02-19 22:23:54 -0500

    changed prints

--- aion/generate_coefficients.py
+++ aion/generate_coefficients.py
@@ -31,20 +31,39 @@ def optimal_values(trajectory: Trajectory):
 def return_coefficients():
     cases = [
         Trajectory(x, y)
-        for y in np.arange(min_y, max_y, 0.4)
-        for x in np.arange(min_x, max_x, 0.4)
+        for y in np.arange(min_y, max_y, 0.2)
+        for x in np.arange(min_x, max_x, 0.2)
     ]
-    results = np.array(
-        multiprocessing.Pool(multiprocessing.cpu_count() // 3).map(
-            optimal_values, cases
-        )
-    )
+    results = np.array(multiprocessing.Pool().map(optimal_values, cases))
 
-    velocity_fit = curve_fit(f, (results[:, 2], results[:, 3]), results[:, 0])
+    velocity_fit, _ = curve_fit(f, (results[:, 2], results[:, 3]), results[:, 0])
     pitch_fit, _ = curve_fit(f, (results[:, 2], results[:, 3]), results[:, 1])
     return (velocity_fit, pitch_fit)
 
 
+def coeffs_to_string(coeffs):
+    return (
+        str(coeffs[0])
+        + " + "
+        + str(coeffs[1])
+        + " * x + "
+        + str(coeffs[2])
+        + " * y + "
+        + str(coeffs[3])
+        + " * x ** 2 + "
+        + str(coeffs[4])
+        + " * y ** 2 + "
+        + str(coeffs[5])
+        + " * x * y"
+    )
+
+
 # the code doesn't work without this
 if __name__ == "__main__":
-    print(return_coefficients())
+    vel_coeffs, pitch_coeffs = return_coefficients()
+    print(
+        "velocity:\n"
+        + coeffs_to_string(vel_coeffs)
+        + "\npitch:\n"
+        + coeffs_to_string(pitch_coeffs)
+    )
