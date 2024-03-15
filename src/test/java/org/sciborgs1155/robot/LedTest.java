a58458810ca3172c6ab256884ea043642419e900
HenryLi-0 <bxsci8266@gmail.com>
2024-03-15 15:51:54 -0400

    ledtest copy

---
+++ src/test/java/org/sciborgs1155/robot/LedTest.java
@@ -0,0 +1,43 @@
+package org.sciborgs1155.robot;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.sciborgs1155.lib.TestingUtil.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.sciborgs1155.robot.led.LedStrip;
+import org.sciborgs1155.robot.led.LedStrip.LEDTheme;
+
+public class LedTest {
+  LedStrip led;
+  String temp = "";
+  String correct =
+      "[#A9A9A9,#FFFF00,#A9A9A9,#FFFF00,#A9A9A9,#FFFF00,#A9A9A9,#FFFF00,#A9A9A9,#FFFF00,#A9A9A9,#FFFF00,#A9A9A9,#FFFF00,#A9A9A9,#FFFF00,#A9A9A9,#FFFF00,#A9A9A9,#FFFF00,#A9A9A9,#FFFF00,#A9A9A9,#FFFF00,#A9A9A9,#FFFF00,#A9A9A9,#FFFF00,#A9A9A9,#FFFF00]";
+  String latest = "";
+
+  @BeforeEach
+  public void setup() {
+    setupHAL();
+    led = new LedStrip();
+  }
+
+  @Test
+  public void testThemeSet() {
+    assertEquals(correct, LedStrip.getBufferDataString(LEDTheme.SCIBORGS.ledBuffer.get()));
+  }
+
+  @Test
+  public void testRainbowTheme() {
+    var rainbow1 = LedStrip.getBufferDataString(LEDTheme.RAINBOW.ledBuffer.get());
+    fastForward(2);
+    var rainbow2 = LedStrip.getBufferDataString(LEDTheme.RAINBOW.ledBuffer.get());
+    assertNotEquals(rainbow1, rainbow2);
+  }
+
+  @AfterEach
+  public void destroy() {
+    led.close();
+  }
+}
