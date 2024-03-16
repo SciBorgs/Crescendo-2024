f90ddec42e1d0cc587ca7e4f876be2798c5cfd75
ethan jones <jonese>
2024-03-16 10:27:32 -0400

    siggy makes me do this

--- src/main/java/org/sciborgs1155/lib/SwerveUtils.java
+++ src/main/java/org/sciborgs1155/lib/SwerveUtils.java
@@ -11,37 +11,44 @@ public class SwerveUtils {
       double deltaTime) {
     // acceleration we give it that is potentially bad
     double givenMaxAcceleration = Double.POSITIVE_INFINITY;
-    double associatedSpeed = 0;
-    double associatedPrevSpeed = 0;
+    double associtatedVelocity = 0;
+    double associatedPrevVelocity = 0;
     for (int i = 0; i < moduleStates.length; i++) {
       double currAcceleration =
-      Math.abs(moduleStates[i].speedMetersPerSecond
-              - prevModuleStates[i].speedMetersPerSecond / deltaTime);
+          Math.abs(
+              (moduleStates[i].speedMetersPerSecond
+                  - prevModuleStates[i].speedMetersPerSecond) / deltaTime);
       if (currAcceleration < attainableMaxAcceleration) {
         continue;
       }
       if (currAcceleration < givenMaxAcceleration) {
         givenMaxAcceleration = currAcceleration;
-        associatedSpeed = moduleStates[i].speedMetersPerSecond;
-        associatedPrevSpeed = prevModuleStates[i].speedMetersPerSecond;
+        associtatedVelocity = moduleStates[i].speedMetersPerSecond;
+        associatedPrevVelocity = prevModuleStates[i].speedMetersPerSecond;
       }
     }
-    if (associatedSpeed == 0) {
-      associatedSpeed = 0.000000000001;
+    if (associtatedVelocity == 0) {
+      associtatedVelocity = 0.000000000001;
     }
     if (givenMaxAcceleration == Double.POSITIVE_INFINITY) {
       return;
     }
-    System.out.println(associatedSpeed);
+    System.out.println(associtatedVelocity);
     double factor =
-        (associatedPrevSpeed
+        (associatedPrevVelocity
                 + MathUtil.clamp(
                     givenMaxAcceleration, -attainableMaxAcceleration, attainableMaxAcceleration))
-            / associatedSpeed;
+            / associtatedVelocity;
     System.out.println(factor);
     if (givenMaxAcceleration > attainableMaxAcceleration) {
       for (int i = 0; i < moduleStates.length; i++) {
-        moduleStates[i].speedMetersPerSecond = moduleStates[i].speedMetersPerSecond * factor;
+        double test = moduleStates[i].speedMetersPerSecond * factor;
+        moduleStates[i].speedMetersPerSecond =
+            MathUtil.clamp(
+                moduleStates[i].speedMetersPerSecond * factor,
+                prevModuleStates[i].speedMetersPerSecond - attainableMaxAcceleration,
+                prevModuleStates[i].speedMetersPerSecond + attainableMaxAcceleration);
+        System.out.println(moduleStates[i].speedMetersPerSecond == test);
       }
     }
   }
