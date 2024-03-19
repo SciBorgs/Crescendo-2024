b473138dd63901ff2b73e1b418f525457ea1d6b9
dannynotsmart <75951607+dannynotsmart@users.noreply.github.com>
2024-03-19 17:31:32 -0400

    shooter cleaned test 1

--- src/main/java/org/sciborgs1155/robot/shooter/Shooter.java
+++ src/main/java/org/sciborgs1155/robot/shooter/Shooter.java
@@ -1,12 +1,15 @@
 package org.sciborgs1155.robot.shooter;
 
-import static edu.wpi.first.units.Units.RadiansPerSecond;
-import static edu.wpi.first.units.Units.Second;
-import static edu.wpi.first.units.Units.Seconds;
-import static edu.wpi.first.units.Units.Volts;
+import static edu.wpi.first.units.Units.*;
 import static org.sciborgs1155.robot.Constants.PERIOD;
+import static org.sciborgs1155.robot.Ports.Shooter.BOTTOM_MOTOR;
+import static org.sciborgs1155.robot.Ports.Shooter.TOP_MOTOR;
 import static org.sciborgs1155.robot.shooter.ShooterConstants.*;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.networktables.DoubleEntry;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -14,56 +17,101 @@ import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import java.util.Optional;
 import java.util.function.DoubleSupplier;
-import monologue.Annotations.Log;
 import monologue.Logged;
 import org.sciborgs1155.lib.InputStream;
+import org.sciborgs1155.lib.Tuning;
 import org.sciborgs1155.robot.Robot;
 import org.sciborgs1155.robot.commands.Shooting;
 
 public class Shooter extends SubsystemBase implements AutoCloseable, Logged {
-  private final ShooterIO shooter;
+  private final WheelIO top;
+  private final WheelIO bottom;
+
+  private double setpoint;
+
+  private final SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(kS, kV, kA);
+
+  private final DoubleEntry p = Tuning.entry("/Robot/shooter/P", kP);
+  private final DoubleEntry i = Tuning.entry("/Robot/shooter/I", kI);
+  private final DoubleEntry d = Tuning.entry("/Robot/shooter/D", kD);
+
+  private final PIDController topPID = new PIDController(kP, kI, kD);
+  private final PIDController bottomPID = new PIDController(kP, kI, kD);
+
   private final SysIdRoutine sysId;
 
-  /** Creates real or simulated shooter based on {@link Robot#isReal()}. */
   public static Shooter create() {
-    return Robot.isReal() ? new Shooter(new RealShooter()) : new Shooter(new SimShooter());
+    return Robot.isReal()
+        ? new Shooter(new RealWheel(TOP_MOTOR), new RealWheel(BOTTOM_MOTOR))
+        : new Shooter(new SimWheel(), new SimWheel());
   }
 
-  /** Creates a fake shooter. */
   public static Shooter none() {
-    return new Shooter(new NoShooter());
+    return new Shooter(new NoWheel(), new NoWheel());
   }
 
-  public Shooter(ShooterIO shooter) {
-    this.shooter = shooter;
+  public Shooter(WheelIO top, WheelIO bottom) {
+    this.top = top;
+    this.bottom = bottom;
+
+    top.setInverted(true);
+
     sysId =
         new SysIdRoutine(
             new SysIdRoutine.Config(Volts.per(Second).of(1), Volts.of(10.0), Seconds.of(11)),
-            new SysIdRoutine.Mechanism(v -> shooter.setVoltage(v.in(Volts)), null, this));
+            new SysIdRoutine.Mechanism(v -> setVoltage(v.in(Volts)), null, this));
 
     SmartDashboard.putData("shooter quasistatic backward", quasistaticBack());
     SmartDashboard.putData("shooter quasistatic forward", quasistaticForward());
     SmartDashboard.putData("shooter dynamic backward", dynamicBack());
     SmartDashboard.putData("shooter dynamic forward", dynamicForward());
 
-    setDefaultCommand(run(() -> shooter.setSetpoint(IDLE_VELOCITY.in(RadiansPerSecond))));
+    setDefaultCommand(run(() -> setSetpoint(IDLE_VELOCITY.in(RadiansPerSecond))));
   }
 
-  /**
-   * Run the shooter at a specified velocity.
-   *
-   * @param velocity The desired velocity in radians per second.
-   * @return The command to set the shooter's velocity.
-   */
+  public void setVoltage(double voltage) {
+    top.setVoltage(voltage);
+    bottom.setVoltage(voltage);
+  }
+
+  public double topVelocity() {
+    return top.velocity();
+  }
+
+  public double bottomVelocity() {
+    return bottom.velocity();
+  }
+
+  public void setSetpoint(double velocity) {
+    double ff = feedforward.calculate(setpoint, velocity, PERIOD.in(Seconds));
+    double topOut = topPID.calculate(top.velocity(), velocity);
+    double bottomOut = bottomPID.calculate(bottom.velocity(), velocity);
+    top.setVoltage(MathUtil.clamp(ff + topOut, -12, 12));
+
+    bottom.setVoltage(MathUtil.clamp(ff + bottomOut, -12, 12));
+    setpoint = velocity;
+  }
+
+  public boolean atSetpoint() {
+    return topPID.atSetpoint() && bottomPID.atSetpoint();
+  }
+
+  public double setpoint() {
+    return setpoint;
+  }
+
+  public void updatePID() {
+    topPID.setPID(p.get(), i.get(), d.get());
+    bottomPID.setPID(p.get(), i.get(), d.get());
+  }
+
   public Command runShooter(DoubleSupplier velocity) {
-    return run(() -> shooter.setSetpoint(velocity.getAsDouble()))
-        .withName("running shooter")
-        .asProxy();
+    return run(() -> setSetpoint(velocity.getAsDouble())).withName("running shooter").asProxy();
   }
 
   public Command manualShooter(DoubleSupplier stickInput) {
     return runShooter(
-        InputStream.of(stickInput).scale(10).scale(PERIOD.in(Seconds)).add(shooter::setpoint));
+        InputStream.of(stickInput).scale(10).scale(PERIOD.in(Seconds)).add(this::setpoint));
   }
 
   public Command runShooter(double velocity) {
@@ -74,22 +122,12 @@ public class Shooter extends SubsystemBase implements AutoCloseable, Logged {
     return runShooter(velocity);
   }
 
-  /**
-   * @return Shooter velocity in radians per second
-   */
-  @Log.NT
   public double rotationalVelocity() {
-    return shooter.topVelocity();
+    return top.velocity();
   }
 
-  @Log.NT
   public double tangentialVelocity() {
     return Shooting.flywheelToNoteSpeed(rotationalVelocity());
-  }
-
-  @Log.NT
-  public boolean atSetpoint() {
-    return shooter.atSetpoint();
   }
 
   public Command quasistaticBack() {
@@ -111,10 +149,13 @@ public class Shooter extends SubsystemBase implements AutoCloseable, Logged {
   @Override
   public void periodic() {
     log("command", Optional.ofNullable(getCurrentCommand()).map(Command::getName).orElse("none"));
+
+    updatePID();
   }
 
   @Override
   public void close() throws Exception {
-    shooter.close();
+    top.close();
+    bottom.close();
   }
 }
