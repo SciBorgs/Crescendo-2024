f4049727608cc3816b93cc3cf99c4dec57958f84
sigalrmp <sigalrmp@gmail.com>
2024-03-25 17:11:06 -0400

    more prediction

--- src/main/java/org/sciborgs1155/robot/commands/Shooting.java
+++ src/main/java/org/sciborgs1155/robot/commands/Shooting.java
@@ -139,7 +139,7 @@ public class Shooting implements Logged {
                     && atYaw(yawFromNoteVelocity(calculateNoteVelocity())))
         .deadlineWith(
             drive.drive(
-                vx.scale(0.5), vy.scale(0.5), () -> yawFromNoteVelocity(calculateNoteVelocity(Seconds.of(0.02)))),
+                vx.scale(0.5), vy.scale(0.5), () -> yawFromNoteVelocity(calculateNoteVelocity(Seconds.of(0.1)))),
             pivot.runPivot(() -> pitchFromNoteVelocity(calculateNoteVelocity())));
     // .unless(() -> !canShoot());
   }
