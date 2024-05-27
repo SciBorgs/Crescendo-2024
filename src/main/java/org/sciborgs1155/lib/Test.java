c35c0bb05b80c5842bb48fb2394251a337fe5972
sigalrmp <sigalrmp@gmail.com>
2024-05-26 21:59:16 -0400

    more documentation

--- src/main/java/org/sciborgs1155/lib/Test.java
+++ src/main/java/org/sciborgs1155/lib/Test.java
@@ -6,6 +6,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import java.util.Set;
 
+/** A test, consisting of a command and assertions. Can be used for unit tests or ran on robot. */
 public record Test(Command testCommand, Set<Assertion> assertions) {
   /**
    * @param command
