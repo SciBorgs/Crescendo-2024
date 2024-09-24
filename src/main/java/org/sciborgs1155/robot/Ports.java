904bdd125d3c60d6c94952e4e672e2eae27074cb
jonahk17 <jonahkwon@Jonahs-MacBook-Air-2.local>
2024-09-24 16:09:39 -0400

    Pushed on wrong branch by accident

--- src/main/java/org/sciborgs1155/robot/Ports.java
+++ src/main/java/org/sciborgs1155/robot/Ports.java
@@ -10,6 +10,22 @@ public final class Ports {
   static {
     idToName = new HashMap();
     // idToName.put(id, name);
+    idToName.put(6, "shooter - top motor");
+    idToName.put(9, "shooter - bottom motor");
+    idToName.put(7, "pivot - top left motor");
+    idToName.put(39, "pivot - right bottom motor");
+    idToName.put(51, "pivot - left bottom motor");
+    idToName.put(35, "pivot - right bottom motor");
+    idToName.put(10, "feeder - motor");
+    idToName.put(2, "drive - front left translational motor");
+    idToName.put(4, "drive - rear left translational motor");
+    idToName.put(3, "drive - front right translational motor");
+    idToName.put(1, "drive - rear right translational motor");
+    idToName.put(21, "drive - front left rotational motor");
+    idToName.put(24, "drive - rear left rotational motor");
+    idToName.put(34, "drive - front right rotational motor");
+    idToName.put(20, "drive - rear right rotational motor");
+    idToName.put(5, "intake - motor");
   }
 
   public static final class OI {
