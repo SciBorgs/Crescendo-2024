fb16eb7a0b6d998ed9aa4f5df37c22ebc25e0d87
jonahk17 <jonahkwon@Jonahs-MacBook-Air-2.local>
2024-10-07 17:04:05 -0400

    spotApply

--- src/main/java/org/sciborgs1155/robot/Robot.java
+++ src/main/java/org/sciborgs1155/robot/Robot.java
@@ -231,12 +231,14 @@ public class Robot extends CommandRobot implements Logged {
     operator
         .leftBumper()
         .whileTrue(
-            shooting.shootWithPivot(PivotConstants.UNDERFEED_ANGLE, ShooterConstants.DEFAULT_VELOCITY));
+            shooting.shootWithPivot(
+                PivotConstants.UNDERFEED_ANGLE, ShooterConstants.DEFAULT_VELOCITY));
 
     // shoot 45 deg FAST (far as possible) overfeeding
     operator
         .rightBumper()
-        .whileTrue(shooting.shootWithPivot(PivotConstants.OVERFEED_ANGLE, ShooterConstants.MAX_VELOCITY));
+        .whileTrue(
+            shooting.shootWithPivot(PivotConstants.OVERFEED_ANGLE, ShooterConstants.MAX_VELOCITY));
 
     // operator climb (b)
     operator
